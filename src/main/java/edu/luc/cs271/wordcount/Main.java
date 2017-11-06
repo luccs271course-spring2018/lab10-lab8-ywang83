23c35f0c52b72a0a7944efda05fda0c178ddf7d4
rogerld <ywang83@luc.edu>
2017-11-06 07:05:27 -0600

    update

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -4,20 +4,39 @@ import java.util.*;
 
 public class Main {
 
-  public static void main(final String[] args) throws InterruptedException {
+    public static void main(final String[] args) throws InterruptedException {
 
-    // set up the scanner so that it separates words based on space and punctuation
-    final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
+        // set up the scanner so that it separates words based on space and punctuation
+        final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
+        Map<String, Integer> aMap = new HashMap<>();
+        WordCounter count = new WordCounter(aMap);
 
-    // TODO complete this main program
-    // 1. create a WordCounter instance
-    // 2. use this to count the words in the input
-    // 3. determine the size of the resulting map
-    // 4. create an ArrayList of that size and
-    // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
-    // 6. sort the ArrayList in descending order by count
-    //    using Collections.sort and an instance of the provided comparator (after fixing the latter)
-    // 7. print the (up to) ten most frequent words in the text
+        List<String> list = new ArrayList<String>();
+        while (input.hasNext()) {
+            String[] array = input.nextLine().split(" ");
+            for (int i = 0; i < array.length; i++) {
+                list.add(array[i]);
+            }
+        }
+        System.out.println(list);
+        for (String temp : list) {
+            String word = temp.toLowerCase();
+            if (aMap.containsKey(word)) {
+                int c = aMap.get(word);
+                aMap.put(word, c + 1);
+            } else
+                aMap.put(word, 1);
+        }
 
-  }
+        Set entries = aMap.entrySet();
+        if (entries != null) {
+            Iterator iterator = entries.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                Object key = entry.getKey();
+                Object value = entry.getValue();
+                System.out.println(key + " " + value);
+            }
+        }
+    }
 }
